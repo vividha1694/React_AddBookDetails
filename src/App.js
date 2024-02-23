816bfb071bfde7051ab25343aa0e9e556ecf19c3
vividha1694 <139147831+vividha1694@users.noreply.github.com>
2024-02-23 15:25:49 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,69 @@
-import logo from './logo.svg';
+import React, { useState } from 'react'
 import './App.css';
 
 function App() {
+  
+  const [title,setTitle] = useState();
+  const [author,setAuthor] = useState();
+  const [year,setYear] = useState();
+  const [addList,setAddList] =useState([]);
+
+  const handleAddBook =() => {
+    if(title !== '' && author !== '' && year !== '') {
+      setAddList([...addList, {title,author,year}]);
+      setTitle('');
+      setAuthor('');
+      setYear('');
+    }else{
+      alert('Please fill all the Details');
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <div  className='container'>
+    <div>
+        <label htmlFor=''>Book Title:</label>
+        <input
+          type="text"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}/>
+       </div>
+        <div>
+          <label htmlFor="">Book Author:</label>
+          <input type="text"
+          value={author}
+          onChange={(e) => setAuthor(e.target.value)} />
+        </div>
+        <div>
+          <label htmlFor="">Published Year:</label>
+          <input type="text"
+          value={year}
+          onChange={(e) => setYear(e.target.value)} />
+        </div>
+        <button className="btn" onClick={handleAddBook}>Add Book</button>
+
+        <table className='table'>
+        <thead className='table-section'>
+          <tr>
+            <th>Book Title</th>
+            <th>Book Author</th>
+            <th>Published Year</th>
+          </tr>
+        </thead>
+        <tbody id='book-list'>
+          {addList.map((book,index)=> (
+            <tr key={index}>
+            <td>{book.title}</td>
+            <td>{book.author}</td>
+            <td>{book.year}</td>
+          </tr>
+          ))}
+        </tbody>
+        </table>
+  
     </div>
+    </>
   );
 }
 
